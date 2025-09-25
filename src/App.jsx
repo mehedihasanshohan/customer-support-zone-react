22407e6e10dd9ba6e747a89c690b39729cd93363
mehedi hasan <mhshohan04@gmail.com>
2025-09-25 12:39:14 +0600

    projects initialization with vite+tailwindcssV4

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h2 className='text-red-600 font-extralight text-3xl'>customer support zone</h2>
+    </>
+  )
+}
+
+export default App
